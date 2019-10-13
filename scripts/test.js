6f8f4e014ed6640a1b8b29d2852cdc936c7a08bb
Neel <neelprasad15@gmail.com>
2019-10-13 21:04:06 +0530

    upto Arrow

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,13 @@
 "use strict";
-console.log('hello Hashvanth Kumar');
+// Arrow Function
+var arrow = function (m) { return 5 * m; };
+console.log(arrow(5));
+var obj = {
+    id: 1,
+    greet: function () {
+        var _this = this;
+        setTimeout(function () { console.log(_this.id); }, 1000);
+    }
+};
+obj.greet();
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
-console.log('hello Hashvanth Kumar');
+// Arrow Function
+let arrow = (m:number) => 5*m;
+console.log(arrow(5));
+
+var obj = {
+    id : 1,
+    greet : function() {
+        setTimeout(()=>{console.log(this.id)}, 1000);
+    }
+}
+
+obj.greet();
